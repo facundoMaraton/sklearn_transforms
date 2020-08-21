b09d02e339419a66e0c3279d1e2cf69b33c7d72d
facundoMaraton <69740222+facundoMaraton@users.noreply.github.com>
2020-08-20 22:23:36 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,16 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+# All sklearn Transforms must have the `transform` and `fit` methods
+class NormaliceColumns(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+        
+    def fit(self, X, y=None):
+        return self
 
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        return (data-data.min())/(data.max()-data.min())
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -13,13 +24,3 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         return data.drop(labels=self.columns, axis='columns')
-
-    # All sklearn Transforms must have the `transform` and `fit` methods
-class NormaliceColumns(BaseEstimator, TransformerMixin):
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        return (data-data.min())/(data.max()-data.min())
